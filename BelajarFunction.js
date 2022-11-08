2c1226588f62bf87cad94729cc51960104f8b424
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-08 13:48:40 +0700

    added string, function, branching

---
+++ BelajarFunction.js
@@ -0,0 +1,38 @@
+// camelCase
+// PascalCase
+
+//menggunakan function
+function greetingWords(nama) {
+  console.log(`Perkenalkan, namaku adalah ${nama}`);
+  console.log("Halo guys, saya adalah sebuah function atau method");
+}
+
+greetingWords("Javascript");
+greetingWords("React");
+greetingWords("Next");
+
+//menggunakan const/constanta
+const luasSegitiga = (alas, tinggi) => {
+  let luas = (alas * tinggi) / 2;
+  console.log("Luas dari segitiga adalah :", luas);
+};
+
+luasSegitiga(10, 8);
+
+//memanggil function di dalam function lain (proses callback)
+const funcTest1 = () => {
+  console.log("hello, saya dari function 1");
+};
+
+const functTest2 = () => {
+  funcTest1();
+  console.log("hello, function 1. aku dari function 2");
+};
+
+const functTest3 = () => {
+  functTest2();
+  funcTest1();
+  console.log("hello function 2 dan 1, aku function 3");
+};
+
+functTest3();

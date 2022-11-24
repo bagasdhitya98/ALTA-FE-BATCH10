b8359adb56599aa81132018b248aa8491f2d0178
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-24 12:02:05 +0700

    container

--- vite-hooks/src/App.jsx
+++ vite-hooks/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Container from "./components/Container";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Container></Container>;
 };
 
 export default App;
---
+++ vite-hooks/src/components/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return <div className="w-screen h-screen">{children}</div>;
+};
+
+export default Container;

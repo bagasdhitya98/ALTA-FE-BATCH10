de945036ef0cef97c59b1ed53ee9138a3fda288e
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-25 13:41:32 +0700

    register added

--- vite-hooks/src/pages/Register.jsx
+++ vite-hooks/src/pages/Register.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    alert(name);
+    event.preventDefault();
+  };
+
+  return (
+    <div className="flex flex-row justify-center w-screen h-full bg-white">
+      <div className="mr-60 mt-60 w-1/2 h-full">
+        <img
+          className="ml-20"
+          src="https://academy.alterra.id/wp-content/uploads/2022/06/Logo-Colour-Transparant-1.png"
+          width={400}
+          height={400}
+        />
+      </div>
+      <div className="bg-alta-space-cadet w-screen">
+        <form
+          className="bg-alta-space-cadet p-48"
+          onSubmit={() => handleSubmit()}
+        >
+          <label className="text-white flex flex-row items-center">
+            Name
+            <input
+              className="text-black input input-bordered w-48 max-w-xs bg-white shadow-xl ml-16 my-5"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </label>
+          <label className="text-white flex flex-row items-center">
+            Email
+            <input
+              className="text-black input input-bordered w-48 max-w-xs bg-white shadow-xl ml-16 my-5"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </label>
+          <label className="text-white flex flex-row items-center">
+            Password
+            <input
+              className="text-black input input-bordered w-48 max-w-xs bg-white shadow-xl ml-10 my-5"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </label>
+          <input
+            className="bg-alta-orange w-60 h-10 rounded mt-20 font-semibold rounded-xl ml-10"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;

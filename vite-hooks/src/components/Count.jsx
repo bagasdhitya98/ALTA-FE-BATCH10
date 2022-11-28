18ccc1a24f8538b2814205897f10e1bc2d1bcbc0
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-28 16:09:36 +0700

    added handle button

--- vite-hooks/src/components/Count.jsx
+++ vite-hooks/src/components/Count.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useCountContext } from "../context/context";
 
-const Count = ({ count }) => {
+const Count = () => {
+  const { count, handleCount } = useCountContext();
+
   return (
     <>
       <div className="flex-col">
         <h1 className="text-white">{count}</h1>
-        <button className="btn btn-secondary">-</button>
-        <button className="btn btn-secondary">Reset</button>
-        <button className="btn btn-secondary">+</button>
+        <button className="btn btn-secondary" onClick={() => handleCount(-1)}>
+          -
+        </button>
+        <button className="btn btn-secondary" onClick={() => handleCount(0)}>
+          Reset
+        </button>
+        <button className="btn btn-secondary" onClick={() => handleCount(1)}>
+          +
+        </button>
       </div>
     </>
   );

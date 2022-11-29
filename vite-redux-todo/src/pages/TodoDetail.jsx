a0b233e48fee926a4e1e367ed7acb1eb01c6403c
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-29 14:10:38 +0700

    add router & tailwind

---
+++ vite-redux-todo/src/pages/TodoDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoDetail = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TodoDetail;
---
+++ vite-redux-todo/src/pages/TodoPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoPage = () => {
+  return (
+    <div className="w-screen h-screen bg-alta-orange">
+      <div></div>
+    </div>
+  );
+};
+
+export default TodoPage;
